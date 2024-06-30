de4074831103febf9eea9e2c7f3d86d3e8df6402
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-30 15:48:13 +0530

    hello world

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,5 +1,3 @@
-import 'package:hello_world/hello_world.dart' as hello_world;
-
-void main(List<String> arguments) {
-  print('Hello world: ${hello_world.calculate()}!');
+void main(){
+  print("Hello, World!");
 }
